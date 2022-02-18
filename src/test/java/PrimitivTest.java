c6fb1c6fe3630f5227f49e199595386006051985
Mazur <Slavaslidchum@gmail.com>
2022-02-18 14:22:04 +0200

    Initial commit

---
+++ src/test/java/PrimitivTest.java
@@ -0,0 +1,9 @@
+import org.apache.http.util.Asserts;
+import org.junit.jupiter.api.Test;
+
+public class PrimitivTest {
+    @Test
+    public void test(){
+        Asserts.check(true, "Good true");
+    }
+}
